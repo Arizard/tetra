9cee206fa7ea13ff0c6e83cea832298464e66dca
Arie <aoldman@deputy.com>
2019-11-19 12:43:14 +1100

    add test for transformcsv with multiple transforms

---
+++ transform_test.go
@@ -0,0 +1,46 @@
+package tetra
+
+import "testing"
+
+func TestTransformCSV(t *testing.T) {
+	config := Config{
+		Comma:           ',',
+		FieldsPerRecord: -1,
+	}
+	config.AddTransform(
+		"none",
+		map[string]interface{}{},
+	)
+	config.AddTransform(
+		"slice_rows",
+		map[string]interface{}{
+			"start": "1",
+			"end":   "-1",
+		},
+	)
+	type args struct {
+		cfg Config
+		csv string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			"Test 1",
+			args{
+				config,
+				"a,b,c,d,\n1,2,3,4,\ne,f,g,h,\n5,6,7,8,\n",
+			},
+			"1,2,3,4,\ne,f,g,h,\n5,6,7,8,\n",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := TransformCSV(tt.args.cfg, tt.args.csv); got != tt.want {
+				t.Errorf("TransformCSV() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
