569f33e7213cc7edbd71ef6fe8a1629c6098b51e
Arie <aoldman@deputy.com>
2020-02-17 15:53:18 +1100

    add ignore row and merge columns operation

--- operations.go
+++ operations.go
@@ -6,6 +6,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"io"
+	"math"
 	"strings"
 )
 
@@ -139,11 +140,85 @@ func titleCaseColumnOp(transform Transform, csvData string) (string, error) {
 	return b.String(), nil
 }
 
+func max(x, y int) int {
+	return int(math.Max(float64(x), float64(y)))
+}
+
+func mergeColumnsOp(transform Transform, csvData string) (string, error) {
+
+	var recordsOut [][]string
+
+	reader := readerFromData(csvData, transform)
+
+	colSrcIndex := int(transform.KWArgs["index_from"].(float64))
+	colDstIndex := int(transform.KWArgs["index_to"].(float64))
+
+	recordsIn, err := reader.ReadAll()
+	if err != nil {
+		return "", err
+	}
+
+	for _, record := range recordsIn {
+		newRecord := record
+		if len(newRecord) - 1 <= max(colSrcIndex, colDstIndex) {
+			srcCell := newRecord[colSrcIndex]
+			if srcCell != "" {
+				newRecord[colDstIndex] = srcCell
+			}
+		}
+		recordsOut = append(recordsOut, newRecord)
+	}
+
+	var b bytes.Buffer
+
+	csvDataBuf := bufio.NewWriter(&b)
+
+	csvWriter := csv.NewWriter(csvDataBuf)
+	csvWriter.WriteAll(recordsOut)
+	csvWriter.Flush()
+
+	return b.String(), nil
+}
+
+func ignoreRowsWhereColumnEqualsOp(transform Transform, csvData string) (string, error) {
+
+	var recordsOut [][]string
+
+	reader := readerFromData(csvData, transform)
+
+	colIndex := int(transform.KWArgs["index"].(float64))
+	colMatch := transform.KWArgs["match"].(string)
+
+	recordsIn, err := reader.ReadAll()
+	if err != nil {
+		return "", err
+	}
+
+	for _, record := range recordsIn {
+		newRecord := record
+		if len(newRecord) - 1 > colIndex { continue }
+		if newRecord[colIndex] == colMatch { continue }
+		recordsOut = append(recordsOut, newRecord)
+	}
+
+	var b bytes.Buffer
+
+	csvDataBuf := bufio.NewWriter(&b)
+
+	csvWriter := csv.NewWriter(csvDataBuf)
+	csvWriter.WriteAll(recordsOut)
+	csvWriter.Flush()
+
+	return b.String(), nil
+}
+
 var operationMap = map[string](func(Transform, string) (string, error)){
 	"none":             noneOp,
 	"slice_rows":       sliceRowsOp,
 	"reverse_rows":     reverseRowsOp,
 	"titlecase_column": titleCaseColumnOp,
+	"merge_columns": mergeColumnsOp,
+	"ignore_rows_where_column_equals": ignoreRowsWhereColumnEqualsOp,
 }
 
 func operate(transform Transform, csvData string) (string, error) {
